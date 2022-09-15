f8b7804cd6ae123ee626d3d7b327d3fe8309a5b9
Oscar Thelander <oscarga.thelander@gmail.com>
2022-09-15 21:54:59 +0200

    Renamed file

--- src/set_1_1.py
+++
@@ -1,8 +0,0 @@
-# Convert hex to base64
-import codecs
-
-def hex_to_base64(hex):
-    return codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
-
-text = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-print(hex_to_base64(text))
